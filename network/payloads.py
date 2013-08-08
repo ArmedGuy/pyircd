9da056c2d5fb1b88ad40848eceeeb99e49b5b88e
Johan Jatko <johan@pie-studios.com>
2013-08-08 11:48:49 +0200

    Fix some retarded errors

--- network/payloads.py
+++ network/payloads.py
@@ -1,7 +1,7 @@
-from * import commands
+from irc.commands import *
 class OnUserConnect():
     def __init__(self, user):
-        user.send(RPL_WELCOME(user))
-        user.send(RPL_YOURHOST(user))
-        user.send(RPL_CREATED(user))
-        user.send(RPL_MYINFO(user))
+        user.send(RPL_WELCOME(user).ToString())
+        user.send(RPL_YOURHOST(user).ToString())
+        user.send(RPL_CREATED(user).ToString())
+        user.send(RPL_MYINFO(user).ToString())
